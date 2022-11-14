fd89121826339cec93afd7c81543595bccc8ecfa
iRupom <rakib.rupom007@gmail.com>
2022-11-14 23:34:04 +0600

    string complete

---
+++ String/string.js
@@ -0,0 +1,17 @@
+var a = 'abc';
+var b = 'bcd';
+
+// console.log(a < b);
+console.log('001' == 1);
+
+var c = a.concat(' ','rakib');
+console.log(c);
+
+// string methods 
+/** 
+ * charAt
+ * concat
+ * substr
+ * startsWith
+ * endsWith
+ */
